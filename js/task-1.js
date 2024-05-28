ae1364ab6700892a46e4e081592a5b5f98a88975
CactusW <chernyavskij5@gmail.com>
2024-05-28 11:11:19 +0300

    fix_1task

--- js/task-1.js
+++ js/task-1.js
@@ -1,9 +1,11 @@
 function makeTransaction(quantity, pricePerDroid, customerCredits) {
     const totalPrice = quantity * pricePerDroid;
-    if (totalPrice > customerCredits)
+    if (totalPrice > customerCredits) {
         return "Insufficient funds!";
-    else customerCredits => totalPrice;
-        return `You ordered ${quantity} droids worth ${totalPrice} credits!`  
+    } else {
+        customerCredits -= totalPrice;
+        return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
+    }
 }
 
 console.log(makeTransaction(5, 3000, 23000)); // "You ordered 5 droids worth 15000 credits!"
